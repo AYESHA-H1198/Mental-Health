5dd3eda6439f55f6f3e814c73b796e32dc5f3d17
Ayesha Fayyaz <ayeshafayax3@gmail.com>
2025-06-29 19:26:38 +0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,46 +1,27 @@
 <?php
 
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Check for maintenance mode
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());<?php
-
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// Check for maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
-
-// Register Composer autoloader
+// Autoload dependencies
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-// Run the app
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+// Handle the request
+$kernel = $app->make(Kernel::class);
 
 $response = $kernel->handle(
     $request = Request::capture()
 )->send();
 
 $kernel->terminate($request, $response);
-
